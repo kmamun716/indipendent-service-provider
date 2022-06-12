aacc71c89ea286aac308377642e34b358d07aadd
Kazi Mamun <kazimamun716@gmail.com>
2022-06-12 07:36:09 +0600

    project initialized

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYVHIsXQIu5E9Pw3QmGYk_5GU4pnxTrPg",
+  authDomain: "autobiography-writer.firebaseapp.com",
+  projectId: "autobiography-writer",
+  storageBucket: "autobiography-writer.appspot.com",
+  messagingSenderId: "588338725941",
+  appId: "1:588338725941:web:25bd2ebc7f11a02fe3dd7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default app;
