a1a63a9cd3f969a80c8937210297060edfb10ddf
Kazi Mamun <kazimamun716@gmail.com>
2022-06-15 17:47:54 +0600

    navbar added with condition in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,36 @@
 import { signOut } from 'firebase/auth';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Link } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
+import { useLocation } from 'react-router-dom';
 import auth from "../../firebase.config";
 const Header = () => {
     const [user] = useAuthState(auth);
+    const location = useLocation();
+    console.log(location)
     return (
         <header>
-            <Link to="/checkout">Checkout</Link><br/>
-            {
-                user?<button onClick={()=>signOut(auth)}>Log Out</button> : <Link to='/login'>Login</Link>
-            }
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Navbar</Navbar.Brand>
+                    </LinkContainer>
+                    <Nav className="me-auto">
+                        <LinkContainer to='/about-us'>
+                            <Nav.Link>About</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/checkout'>
+                            <Nav.Link>Checkout</Nav.Link>
+                        </LinkContainer>
+                        {
+                            user ? <Nav.Link onClick={()=>signOut(auth)}>Log Out</Nav.Link> : 
+                            location.pathname!=='/login' && <LinkContainer to='/login'>
+                                <Nav.Link>Login</Nav.Link>
+                            </LinkContainer>
+                        }
+                    </Nav>
+                </Container>
+            </Navbar>
         </header>
     );
 };
